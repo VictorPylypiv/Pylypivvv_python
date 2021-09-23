93251e119509ef9f3933841eb00935ab72f250a0
Victor Pylypiv <pylypivvv@gmail.com>
2021-09-23 08:51:51 +0300

    'task_Traffic_light'

---
+++ Pylypiv_01_PythonStarter/Pylypiv_traffic_light_1.py
@@ -0,0 +1,36 @@
+# На основе примера создайте алгоритм светофора для пешехода.
+# Попросите пользователя ввести количество секунд ожидания.
+# Если пользователь вводит некорректное количество секунд,
+# то запросите ввод еще раз.
+# Каждую итерацию цикла отнимайте единицу от счетчика секунд ожидания,
+# когда счетчик ожидания станет равен нулю, то пешеход может идти.
+
+
+def get_t():
+    t = int(input('From 15 to 60 seconds: '))
+    while True:
+        if 15 <= t <= 60:
+            return t
+        else:
+            t = int(input('fr 15 to 60: '))
+
+
+def tr_l_ped(seconds):
+    wait = seconds
+    light = 'red'
+    while light == 'red':
+        if wait > 0:
+            print(f'wait {wait} sec')
+            wait -= 1
+        else:
+            light = 'green'
+            print('go')
+
+
+def main():
+    t = get_t()
+    tr_l_ped(t)
+
+
+if __name__ == '__main__':
+    main()
